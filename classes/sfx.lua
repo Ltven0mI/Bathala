0c0a1403356177aa7cce3ecee3ba98e4cc0353c8
Ltven0mI <8373231+Ltven0mI@users.noreply.github.com>
2019-10-09 20:52:51 +1000

    Made Sfx use multiple sources

--- classes/sfx.lua
+++ classes/sfx.lua
@@ -4,10 +4,15 @@ local Vector = require "hump.vector"
 local Sfx = Class{
     init = function(self, path)
         self.soundData = love.sound.newSoundData(path)
-        self.source = love.audio.newQueueableSource(self.soundData:getSampleRate(), self.soundData:getBitDepth(), self.soundData:getChannelCount(), 8)
+        self.source1 = love.audio.newQueueableSource(self.soundData:getSampleRate(), self.soundData:getBitDepth(), self.soundData:getChannelCount(), 8)
+        self.source2 = love.audio.newQueueableSource(self.soundData:getSampleRate(), self.soundData:getBitDepth(), self.soundData:getChannelCount(), 8)
         self.play = function(self)
-            self.source:queue(self.soundData)
-            self.source:play()
+            local source = self.source1
+            if self.source1:getFreeBufferCount() < self.source2:getFreeBufferCount() then
+                source = self.source2
+            end
+            source:queue(self.soundData)
+            source:play()
         end
     end,
 }
