3b3d10355a53395d9b56899a5d92adb9d23eaff1
Ltven0mI <8373231+Ltven0mI@users.noreply.github.com>
2019-10-10 13:08:43 +1000

    Implemented Curse Powerup animation

--- assets/entities/curse_powerup.lua
+++ assets/entities/curse_powerup.lua
@@ -2,6 +2,8 @@ local Class = require "hump.class"
 local Vector = require "hump.vector"
 local Signal = require "hump.signal"
 
+local Peachy = require "lib.peachy"
+
 local UseItem = require "assets.entities.use_item"
 
 local CurseProjectile = require "assets.entities.curse_projectile"
@@ -9,6 +11,7 @@ local CurseProjectile = require "assets.entities.curse_projectile"
 local CursePowerup = Class{
     init = function(self, x, y)
         UseItem.init(self, x, y, 16, 16)
+        self.animation = Peachy.new("assets/powerups/curse_powerup.json", love.graphics.newImage("assets/powerups/curse_powerup.png"), "idle")
     end,
     __includes = {
         UseItem
@@ -16,19 +19,18 @@ local CursePowerup = Class{
 
     isUsable=true,
 
-    img = love.graphics.newImage("assets/powerups/curse_powerup_temp.png"),
     icon = love.graphics.newImage("assets/powerups/curse_powerup_icon.png"),
 
     type = "pickupable",
 }
 
 function CursePowerup:update(dt)
-
+    self.animation:update(dt)
 end
 
 function CursePowerup:draw()
     love.graphics.setColor(1, 1, 1, 1)
-    love.graphics.draw(self.img, self.pos.x, self.pos.y)
+    self.animation:draw(self.pos.x, self.pos.y)
 end
 
 function CursePowerup:use(map, x, y, dir)
